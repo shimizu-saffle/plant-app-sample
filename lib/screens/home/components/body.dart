7414436e5877f49abd42e1c81088160542c255be
shimizu-saffle <shimizu.saffle@gmail.com>
2022-06-03 08:27:20 +0900

    TitleWithCustomUnderLine

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:plant_app_sample/constants.dart';
+
+import '../../../components/header_with_search_box.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -8,94 +9,46 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        HeaderWithSearchBox(size: size),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBox(size: size),
+          const TitleWithCustomUnderLine(),
+        ],
+      ),
     );
   }
 }
 
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
+class TitleWithCustomUnderLine extends StatelessWidget {
+  const TitleWithCustomUnderLine({
     Key? key,
-    required this.size,
   }) : super(key: key);
-
-  final Size size;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: size.height * 0.2,
+      height: 24,
       child: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.only(
-              left: kDefaultPadding,
-              right: kDefaultPadding,
-              bottom: 36 + kDefaultPadding,
-            ),
-            height: size.height * 0.2 - 27,
-            decoration: const BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
+          const Padding(
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              'Recommended',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  'Hi Uishopy!',
-                  style: Theme.of(context).textTheme.headline5!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                const Spacer(),
-                Image.asset('assets/images/logo.png')
-              ],
             ),
           ),
           Positioned(
             bottom: 0,
-            right: 0,
             left: 0,
+            right: 0,
             child: Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  )
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: 'Search',
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset('assets/icons/search.svg')
-                ],
-              ),
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
             ),
           )
         ],
